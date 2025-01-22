be174482f04b4bd06b64f59f64e17a6e3d3cdbf9
liav2002 <liavmail00@gmail.com>
2025-01-22 11:52:42 +0200

    add google docstring to 'train_model' function.

--- src/scripts/train.py
+++ src/scripts/train.py
@@ -4,9 +4,15 @@ from src.utils.config_loader import load_config
 from config.consts import CONFIG_PATH
 
 
-def train_model(run_name=None):
+def train_model(run_name: str) -> None:
     """
     Train the Siamese Network using the Trainer class.
+
+    Args:
+        run_name (str): The name of the training run.
+
+    Returns:
+        None
     """
     config = load_config(CONFIG_PATH)
     logger = get_logger(config)
@@ -15,3 +21,4 @@ def train_model(run_name=None):
     logger.start_run(run_name=run_name)
     trainer.train()
     logger.end_run()
+
