8cc3dbbc778f01b7fa27fe3e6dd42f4cbaf105d6
liav2002 <liavmail00@gmail.com>
2025-01-27 06:15:20 -0500

    run train with best params BCE and Loss Greedy

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.scripts.test import test_model
 
 
 def main():
-    train_model(run_name="Siamese_Training_Bayesian_Search")
+    train_model(run_name="Siamese_Last_Train_Best_Params_BCE_Loss_Greedy")
 
 
 if __name__ == "__main__":
